548b2a1e5cba95e2fe4c4ac519e1060f9d8b5efe
ela26 ela26 <m.eljasiewicz@gmail.com>
2023-08-28 08:44:32 +0200

    fixed stories

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,8 +1,8 @@
-import { Header, HeaderProps } from "../components/Header/Header";
+import { Header } from "../components/Header/Header";
 
 export default {
 	title: "Header",
 	component: Header,
 };
 
-export const Default = (args: HeaderProps) => <Header {...args} />;
+export const Default = () => <Header />;
